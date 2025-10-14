6490dfc476815c45cd8cfeca3c900e84e26d3b75
Vinayak Shukla <151124329+vshukla7@users.noreply.github.com>
2025-10-14 10:05:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, jsonify
+from AI.agent import Prompt_Taker
+
+app = Flask(__name__)
+
+@app.route('/<prompt>', methods=['GET'])
+def main(prompt):
+    result = Prompt_Taker(prompt)
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
